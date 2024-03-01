48786e7f5d6656ab7fcb9497f3e5893dc02f639b
gTamietti <131299325+gTamietti@users.noreply.github.com>
2024-03-01 06:48:56 -0300

    Add files via upload

---
+++ Ex1.java
@@ -0,0 +1,21 @@
+package Ex;
+import java.util.Scanner;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número:");
+        double numero1 = scanner.nextDouble();
+
+        System.out.println("Digite o segundo número:");
+        double numero2 = scanner.nextDouble();
+
+        double soma = numero1 + numero2;
+
+        System.out.println("A soma dos dois números é: " + soma);
+
+        scanner.close(); 
+
+}
+}
